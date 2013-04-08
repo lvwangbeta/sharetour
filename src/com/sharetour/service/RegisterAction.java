c006ed8c937656321e5f60b1c281cae8d400eee1
gavin <lvwangbeta@gmail.com>
2013-04-08 23:15:05 +0800

    RegisterAction

---
+++ src/com/sharetour/service/RegisterAction.java
@@ -0,0 +1,51 @@
+package com.sharetour.service;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.sharetour.model.UserInfo;
+import com.sharetour.util.Action;
+
+public class RegisterAction implements Action{
+	
+	private static final String TIP 		 = "tip";
+	private static final String USERNAMEEXIST = "用户名已存在";
+	private static final String CONFIRMERROR = "";
+	private static final String EMAILVALIDATE= "邮箱地址不合法";
+	private static final String EMAILEXIST	 = "该邮箱有注册";
+	
+	@Override
+	public String execute(HttpServletRequest request) {
+		try {
+			request.setCharacterEncoding("utf-8");
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("encode error");
+		}
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		String gender = request.getParameter("gender");
+		String year = request.getParameter("year");
+		String month = request.getParameter("month");
+		String day = request.getParameter("day");
+		String birth = year + "-" + month + "-" + day;
+		
+		boolean username_validate = UserService.checkUsername(username);
+		if(username_validate == false){
+			request.setAttribute(TIP, USERNAMEEXIST);
+			return "register";
+		}	
+		UserInfo user = new UserInfo();
+		user.setUsername(username);
+		user.setPassword(password);
+		user.setEmail(email);
+		user.setGender(Integer.parseInt(gender));
+		user.setBirth(birth);
+		user.Save();
+		request.getSession().setAttribute("user", user);
+		return "home";
+		
+	}
+
+}
