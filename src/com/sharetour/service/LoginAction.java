9c2fb53f8500be7bb369899a8782065a4c7ef599
gavin <lvwangbeta@gmail.com>
2013-04-08 23:38:53 +0800

    LoginAction

---
+++ src/com/sharetour/service/LoginAction.java
@@ -0,0 +1,35 @@
+package com.sharetour.service;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.sharetour.model.UserInfo;
+import com.sharetour.util.Action;
+
+public class LoginAction implements Action{
+	
+	@Override
+	public String execute(HttpServletRequest request) {
+		try {
+			request.setCharacterEncoding("utf-8");
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("encode error");
+		}
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		UserInfo user = UserService.loginCheck(username, password);
+		
+		if(user == null){
+			request.setAttribute(TIP, PWDERROR);
+			return "login";
+		}
+		else{
+			request.getSession().setAttribute("user", user);
+			return "home";
+		}
+		
+		
+	}
+
+}
