b30fa47c1efc7a0ec60601a0ef2278ce68d6d1f0
gavin <lvwangbeta@gmail.com>
2013-04-25 20:17:00 +0800

    PostService and HotPostService log

--- src/com/sharetour/service/HotPostService.java
+++ src/com/sharetour/service/HotPostService.java
@@ -4,13 +4,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.sharetour.cache.CacheHelper;
 import com.sharetour.dao.PostDAO;
 import com.sharetour.model.Post;
 
 public class HotPostService{
 	private final static String ORDER = "likes";
+	private final static String HOTPOST_CACHE_NAME = Post.class.getSimpleName(); 
 	private final static String HOT_KEY = "hot_posts";
+	private static Log log = LogFactory.getLog(HotPostService.class);
 	private int limit = 10;
 	private PostDAO hpdao;
 	public HotPostService(){
@@ -31,21 +36,27 @@ public class HotPostService{
 	 * @return
 	 */
 	public List<Post> getHotPost(int page, int limit){
+		log.info("get hot post from "+page+" limit "+limit);
 		@SuppressWarnings("unchecked")
 		Map<Integer, List<Post>> page_posts_map = (HashMap<Integer, List<Post>>) CacheHelper.
-		getCacheData(Post.class.getSimpleName(), HOT_KEY);
+		getCacheData(HOTPOST_CACHE_NAME, HOT_KEY);
 		List<Post> list = null;
 		if(page_posts_map == null){
+			log.info("get hot post from db");
 			list = hpdao.getPostList(page, limit, ORDER);
 			page_posts_map = new HashMap<Integer, List<Post>>();
 			page_posts_map.put(page, list);
-			CacheHelper.put(Post.class.getSimpleName(), HOT_KEY, page_posts_map);
+			CacheHelper.put(HOTPOST_CACHE_NAME, HOT_KEY, page_posts_map);
 		}
 		else{
 			list = page_posts_map.get(page);
 			if(list == null){
+				log.info("get hot post from db");
 				list = hpdao.getPostList(page, limit, ORDER);
 				page_posts_map.put(page, list);
+			}
+			else{
+				log.info("get hot post from cache");
 			}
 		}
 		return list;
--- src/com/sharetour/service/PostService.java
+++ src/com/sharetour/service/PostService.java
@@ -5,6 +5,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.sharetour.cache.CacheHelper;
 import com.sharetour.dao.PostDAO;
 import com.sharetour.model.Post;
@@ -18,7 +21,11 @@ public class PostService {
 	private final static String POST_CACHE_NAME = Post.class.getSimpleName();
 	private final static String COMMENT_CACHE_NAME = PostComment.class.getSimpleName();
 	private final static String USER_POSTS = "user_posts";
+	private final static String SINGLE_POST = "single_post";
 	private final static String POST_COMMENTS = "post_comments";
+	
+	private static Log log = LogFactory.getLog(PostService.class);
+	
 	/*
 	 * 从本地数据库获得Post对象
 	 * @param post id
@@ -38,22 +45,25 @@ public class PostService {
 	 * @return Post
 	 */
 	public Post getPostById(Long id){
+		log.info("get post by id:"+id);
 		@SuppressWarnings("unchecked")
-		Map<Long, List<Post>> posts_map = (HashMap<Long, List<Post>>) CacheHelper.
-				  getCacheData(POST_CACHE_NAME, USER_POSTS);
-			
-		if(posts_map != null){
-			List<Post> plist = posts_map.get(id);
-			if(plist != null){
-				for(Post post:plist){
-					if(post.getId() == id){
-						return post;
-					}
+		List<Post> plist = (List<Post>) CacheHelper.getCacheData(POST_CACHE_NAME, SINGLE_POST);
+		if(plist != null){
+			for(Post post:plist){
+				if(post.getId() == id){
+					log.info("get post "+id+" from cache");
+					return post;
 				}
-				plist.add(getPostFromDB(id));				
 			}
 		}
-		return getPostFromDB(id);
+		else{
+			plist = new LinkedList<Post>();
+			CacheHelper.put(POST_CACHE_NAME, SINGLE_POST, plist);
+		}
+		log.info("get post "+id+" from db");
+		Post p = getPostFromDB(id);
+		plist.add(p);				
+		return p;
 	}
 	/*
 	 * 保存新post，并放入缓存
@@ -61,8 +71,9 @@ public class PostService {
 	 * @return 
 	 */
 	public boolean savePost(Post post){
-		//保存成功
+		log.info("new post saving...");
 		if(new PostDAO(post).createNewPost()){
+			log.info("new post saved success");
 			@SuppressWarnings("unchecked")
 			Map<Long, List<Post>> posts_map = (HashMap<Long, List<Post>>) CacheHelper.
 											  getCacheData(POST_CACHE_NAME, USER_POSTS);
@@ -77,8 +88,10 @@ public class PostService {
 				posts_map.put(post.getAuthorid(), plist);
 			}
 			plist.add(post);
+			log.info("put new post to cache");
 			return true;
 		}
+		log.error("new post save error");
 		return false;
 	}
 	
@@ -89,7 +102,7 @@ public class PostService {
 	 * @return List<PostComment>
 	 */
 	public List<PostComment> getCommentsByPostId(Long id){
-		
+		log.info("get comments of post:"+id);
 		@SuppressWarnings("unchecked")
 		Map<Long, List<PostComment>> comms_map = (HashMap<Long, List<PostComment>>) 
 					CacheHelper.getCacheData(COMMENT_CACHE_NAME, POST_COMMENTS);
@@ -104,6 +117,10 @@ public class PostService {
 			post.setId(id);
 			clist = new PostDAO(post).getPostComment();			
 			comms_map.put(id, clist);
+			log.info("get commnts of "+id+" from db");
+		}
+		else{
+			log.info("get commnts of "+id+" from cache");
 		}
 		return clist;
 	}
@@ -122,6 +139,7 @@ public class PostService {
 	 * @param authorid
 	 */
 	public List<Post> getPostsOfAuthor(Long authorid){		
+		log.info("get posts list of author:"+authorid);
 		@SuppressWarnings("unchecked")
 		Map<Long, List<Post>> posts_map = (HashMap<Long, List<Post>>) CacheHelper.
 										  getCacheData(POST_CACHE_NAME, USER_POSTS);
@@ -137,6 +155,10 @@ public class PostService {
 			if(plist == null){
 				plist = new PostDAO().getPostsOfAuthor(authorid);
 				posts_map.put(authorid, plist);
+				log.info("get posts list of author:"+authorid+"from db");
+			}
+			else{
+				log.info("get posts list of author:"+authorid+"from cache");
 			}
 		}
 		return plist;
